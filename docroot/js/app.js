6728f26664bb34cddcaeee5a7c10589a46443942
Eric Parker <theriex@gmail.com>
2021-10-29 12:30:31 -0400

    added error log scanning to logcheck.py

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -62,12 +62,12 @@ return {
         const loadfs = diggerapp.modules.map((p) => "js/amd/" + p.name);
         app.amdtimer = {};
         app.amdtimer.load = { start: new Date() };
-        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=211027");
+        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=211029");
     },
 
 
     fileVersion: function () {
-        return "v=211027";  //updated as part of release process
+        return "v=211029";  //updated as part of release process
     },
 
 
