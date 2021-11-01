d762cf64e42fc4552d1a991adb4f2a2b8ea0d34f
Eric Parker <theriex@gmail.com>
2021-11-01 16:35:05 -0400

    v0.9.2

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -62,7 +62,7 @@ return {
         const loadfs = diggerapp.modules.map((p) => "js/amd/" + p.name);
         app.amdtimer = {};
         app.amdtimer.load = { start: new Date() };
-        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=211031");
+        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=211101");
     },
 
 
@@ -70,7 +70,7 @@ return {
 
 
     fileVersion: function () {
-        return "v=211031";  //updated as part of release process
+        return "v=211101";  //updated as part of release process
     },
 
 
