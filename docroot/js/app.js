195eb44df991ad98bc22122d40112be442b75356
Eric Parker <theriex@gmail.com>
2022-04-16 18:45:29 -0400

    Updated slides, release 1.0.0

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -60,7 +60,7 @@ return {
         const loadfs = diggerapp.modules.map((p) => "js/amd/" + p.name);
         app.amdtimer = {};
         app.amdtimer.load = { start: new Date() };
-        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=220413");
+        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=220416");
     },
 
 
@@ -75,7 +75,7 @@ return {
 
 
     fileVersion: function () {
-        return "v=220413";  //updated as part of release process
+        return "v=220416";  //updated as part of release process
     },
 
 
--- server/db.js
+++ server/db.js
@@ -48,7 +48,7 @@ module.exports = (function () {
 
 
     function diggerVersion () {
-        return "v0.9.27";
+        return "v1.0.0";
     }
 
 
