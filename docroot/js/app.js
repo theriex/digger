18ee3f4efc578bfb43a2f2d2b239a45f332741ff
Eric Parker <theriex@gmail.com>
2022-09-07 12:31:24 +1000

    release version v1.0.8 versionCode 8

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -60,7 +60,7 @@ return {
         const loadfs = diggerapp.modules.map((p) => "js/amd/" + p.name);
         app.amdtimer = {};
         app.amdtimer.load = { start: new Date() };
-        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=220822");
+        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=220907");
     },
 
 
@@ -75,7 +75,7 @@ return {
 
 
     fileVersion: function () {
-        return "v=220822";  //updated as part of release process
+        return "v=220907";  //updated as part of release process
     },
 
 
