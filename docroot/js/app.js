d9c5e03fc7eea5480f6412e44a3f30e4e59f72e6
Eric Parker <theriex@gmail.com>
2022-11-24 09:41:33 -1000

    release version v1.0.11 versionCode 12

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -60,7 +60,7 @@ return {
         const loadfs = diggerapp.modules.map((p) => "js/amd/" + p.name);
         app.amdtimer = {};
         app.amdtimer.load = { start: new Date() };
-        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=221107");
+        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=221124");
     },
 
 
@@ -75,7 +75,7 @@ return {
 
 
     fileVersion: function () {
-        return "v=221107";  //updated as part of release process
+        return "v=221124";  //updated as part of release process
     },
 
 
--- server/db.js
+++ server/db.js
@@ -39,7 +39,7 @@ module.exports = (function () {
 
 
     function diggerVersion () {
-        return "v1.0.10";
+        return "v1.0.11";
     }
 
 
