e3d7d7d5ff2b5558e7309c1c34ce1a871ce24809
Eric Parker <theriex@gmail.com>
2023-03-02 08:09:27 -1000

    verbiage change, new server release

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -63,7 +63,7 @@ return {
         const loadfs = diggerapp.modules.map((p) => "js/amd/" + p.name);
         app.amdtimer = {};
         app.amdtimer.load = { start: new Date() };
-        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=230228");
+        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=230301");
     },
 
 
@@ -78,7 +78,7 @@ return {
 
 
     fileVersion: function () {
-        return "v=230228";  //updated as part of release process
+        return "v=230301";  //updated as part of release process
     },
 
 
