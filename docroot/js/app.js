86f0bc09801988919538a3b1066483b0cf3b8c5f
Eric Parker <theriex@gmail.com>
2023-09-14 18:29:20 -0400

    release version 1.2.2, versionCode 25, Build 7

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -64,7 +64,7 @@ return {
         const loadfs = diggerapp.modules.map((p) => "js/amd/" + p.name);
         app.amdtimer = {};
         app.amdtimer.load = { start: new Date() };
-        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=230822");
+        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=230914");
     },
 
 
@@ -79,7 +79,7 @@ return {
 
 
     fileVersion: function () {
-        return "v=230822";  //updated as part of release process
+        return "v=230914";  //updated as part of release process
     },
 
 
--- server/db.js
+++ server/db.js
@@ -39,7 +39,7 @@ module.exports = (function () {
 
 
     function diggerVersion () {
-        return "v1.2.1";
+        return "v1.2.2";
     }
 
 
