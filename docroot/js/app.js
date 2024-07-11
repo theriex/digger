ada96670914df3c499d266f4a7d16c7661828b58
Eric Parker <theriex@gmail.com>
2024-07-11 13:13:30 -0400

    release version 1.3.6, versionCode 39, Build 22

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -117,7 +117,7 @@ return {
         const loadfs = diggerapp.modules.map((p) => "js/amd/" + p.name);
         app.amdtimer = {};
         app.amdtimer.load = { start: new Date() };
-        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=240707");
+        jt.loadAppModules(app, loadfs, app.docroot, init2, "?v=240711");
     },
 
 
@@ -132,7 +132,7 @@ return {
 
 
     fileVersion: function () {
-        return "v=240707";  //updated as part of release process
+        return "v=240711";  //updated as part of release process
     },
 
 
