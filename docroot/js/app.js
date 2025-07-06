241fefa8fa7659798ca95615674ff1869dae8bdc
Eric Parker <theriex@gmail.com>
2025-07-06 13:43:04 -0400

    release version 1.6.2, versionCode 65, Build 48

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -279,7 +279,7 @@ var app = (function () {
             const loadfs = diggerapp.modules.map((p) => "js/amd/" + p.name);
             amdtimer.load.start = new Date();
             jt.loadAppModules(app, loadfs, app.docroot, 
-                              mgrs.boot.initAppModules, "?v=250703"); }
+                              mgrs.boot.initAppModules, "?v=250706"); }
     }; //end mgrs.boot returned access interface
     }());
 
@@ -1064,7 +1064,7 @@ return {
         if(mgrs.pdat.dbObj()) { return mgrs.pdat.songDataVersion(); }
         return app.fileVersion(); },
     fileVersion: function () {
-        return "v=250703";  //updated as part of release process
+        return "v=250706";  //updated as part of release process
     }
 };  //end returned functions
 }());
