51e70fc6de301ff7243e496d22fb1430946a38e2
Eric Parker <theriex@gmail.com>
2025-09-04 09:54:25 -0400

    release version 1.6.42, versionCode 69, Build 52

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -279,7 +279,7 @@ var app = (function () {
             const loadfs = diggerapp.modules.map((p) => "js/amd/" + p.name);
             amdtimer.load.start = new Date();
             jt.loadAppModules(app, loadfs, app.docroot, 
-                              mgrs.boot.initAppModules, "?v=250902"); }
+                              mgrs.boot.initAppModules, "?v=250904"); }
     }; //end mgrs.boot returned access interface
     }());
 
@@ -1078,7 +1078,7 @@ return {
         if(mgrs.pdat.dbObj()) { return mgrs.pdat.songDataVersion(); }
         return app.fileVersion(); },
     fileVersion: function () {
-        return "v=250902";  //updated as part of release process
+        return "v=250904";  //updated as part of release process
     }
 };  //end returned functions
 }());
--- server/db.js
+++ server/db.js
@@ -38,7 +38,7 @@ module.exports = (function () {
 
 
     function diggerVersion () {
-        return "v1.6.41";
+        return "v1.6.42";
     }
 
 
